76e95decd91a457574e5ce51e647f69e6d1687f0
Alessandro DeCredico <adecredico@gmail.com>
2016-05-01 12:29:23 -0700

    Cleanup todo.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+
+
+
+
+function createTable(numCols, numRows){
+  var begin = "<table border=1>";
+  var middle = "";
+  var end = "</table>"
+
+  for (var i = 0; i < numRows; i++){
+    middle += "<tr'>";
+
+    for (var e = 0; e < numCols; e++){
+      middle += "<td></td>";
+    }
+
+    middle += "</tr>";
+  }
+  document.body.innerHTML += "<div>" + begin + middle + end + "</div>"
+}
