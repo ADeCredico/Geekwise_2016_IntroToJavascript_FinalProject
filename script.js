2385739939cb0695bd2ef8a003327a12b855d661
Alessandro DeCredico <adecredico@gmail.com>
2016-05-04 17:27:28 -0700

    Combined to one file for now, fleshed out a bit.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // 'use strict';
 // Conway's Game of Life
 
-//  append a table of numCols by numRows to document.body.innerHTML
-function createTable(numCols, numRows) {
-  const begin = '<table border=1 id="gameTable">';
+//  append a table of numCols by numRows to document.body.innerHTML with the id tableId
+function createTable(numRows, numCols, tableId) {
+  const begin = `<table border=1 id="${tableId}">`;
   let middle = '';
   const end = '</table>';
 
@@ -35,7 +35,7 @@ function create2DArray(rows, columns) {
 
 /** pass a nodelist of cells, number of cells per row, number of rows:
 return array of arrays of the cells */
-function createGridArray(gridCells, cellsPerRowArgument, numberOfRows) {
+function createGridArray(gridCells, cellsPerRow, numberOfRows) {
   const cellArray = create2DArray(numberOfRows);
   let xCoord = 0;
   let yCoord = 0;
@@ -43,7 +43,7 @@ function createGridArray(gridCells, cellsPerRowArgument, numberOfRows) {
   for (let i = 0; i < gridCells.length; i++) {
     cellArray[xCoord][yCoord] = gridCells[i];
 
-    if (cellsPerRowArgument === (yCoord + 1)) {
+    if (cellsPerRow === (yCoord + 1)) {
       xCoord++;
       yCoord = 0;
     }	else {
@@ -54,7 +54,86 @@ function createGridArray(gridCells, cellsPerRowArgument, numberOfRows) {
   return cellArray;
 }
 
-let tableRows = document.getElementsByTagName('tr');
-let tableCells = document.getElementsByTagName('td');
-let tableCellsPerRow = tableCells.length / tableRows.length;
-let simulationSpeed = document.getElementById('simSpeed').value;
+function makeTableFromInput() {
+  const theHeight = document.getElementById('tableHeight');
+  const theWidth = document.getElementById('tableWidth');
+
+  createTable(theHeight, theWidth, 'gameTable');
+}
+
+const gameOfLife = { isPaused: true,
+
+                    ititalize: function initalize(speed, size, density, pauseState) {
+                      self.gameSpeed = speed;
+                      self.gameSize = size;
+                      self.seedDensity = density;
+                      self.isPaused = pauseState;
+                    },
+                    runSimulation: function runSimulation() {
+                      self.gameInterval = setInterval(self.nextGeneration, self.gameSpeed);
+                    },
+                    nextGeneration: function nextGeneration() {
+                      self.gameGrid = self.evaluateLife(self.gameGrid);
+                    },
+                    storeGrid: function storeGrid(grid) {
+                      self.gameGrid = grid;
+                    },
+                    togglePause: function togglePause() {
+                      if (self.isPaused === true) {
+                        self.runSimulation();
+                        self.isPaused = false;
+                      } else {
+                        clearInterval(self.gameInterval);
+                        self.isPaused = true;
+                      }
+                    },
+                    evaluateLife: function evaluateLife(grid) {
+                      /** takes a 2d array and returns the array having
+                       the game of life rules applied*/
+                    },
+                    randomizeSeed: function randomizeSeed() {
+                      // randomly set cells to 'on' using seedDensity
+                    },
+};
+
+function pauseButtonClick() {
+  gameOfLife.togglePause();
+}
+
+function resetButtonClick() {
+  const currentHeight = document.getElementById('tableHeight');
+  const currentWidth = document.getElementById('tableWidth');
+
+  const currentGrid = document.getElementByTagName('table');
+  currentGrid.parentNode.removeChild(currentGrid);
+
+  makeTableFromInput();
+
+  let pageTableRows = document.getElementsByTagName('tr');
+  let pageTableCells = document.getElementsByTagName('td');
+  let pageTableCellsPerRow = pageTableCells.length / pageTableRows.length;
+  let simulationSpeed = document.getElementById('simSpeed').value;
+
+  gameOfLife.storeGrid = createGridArray(pageTableCells,
+                                        pageTableCellsPerRow,
+                                        pageTableRows.length);
+  gameOfLife.initalize();
+  gameOfLife.randomizeSeed();
+  gameOfLife.startSimulation();
+}
+
+function startTheGameOfLife() {
+  makeTableFromInput();
+
+  let pageTableRows = document.getElementsByTagName('tr');
+  let pageTableCells = document.getElementsByTagName('td');
+  let pageTableCellsPerRow = pageTableCells.length / pageTableRows.length;
+  let simulationSpeed = document.getElementById('simSpeed').value;
+
+  gameOfLife.storeGrid = createGridArray(pageTableCells,
+                                        pageTableCellsPerRow,
+                                        pageTableRows.length);
+  gameOfLife.initalize();
+  gameOfLife.randomizeSeed();
+  gameOfLife.startSimulation();
+}
