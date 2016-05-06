5750f7c154ae3fbc8c39ab0bae3cfa6887d8caa3
Alessandro DeCredico <adecredico@gmail.com>
2016-05-05 17:37:20 -0700

    Game logic fleshing out.

--- script.js
+++ script.js
@@ -71,13 +71,62 @@ const gameOfLife = { isPaused: true,
                       gameOfLife.gameGrid = grid;
                       gameOfLife.gameCells = cells;
                     },
+
                     runSimulation: function runSimulation() {
                       gameOfLife.gameInterval = setInterval(gameOfLife.nextGeneration,
                                                             gameOfLife.gameSpeed);
                     },
+
+                    // wraps the assignment operation for the interval
                     nextGeneration: function nextGeneration() {
-                      gameOfLife.gameGrid = gameOfLife.evaluateLife(gameOfLife.gameGrid);
+                      gameOfLife.gameGrid = gameOfLife.evaluateLife();
                     },
+
+                    evaluateLife: function evaluateLife() { // TODO: Make this work
+                      let xCoord = 0;
+                      let yCoord = 0;
+
+                      for (let i = 0; i < gameOfLife.gameHeight.length; i++) {
+                        for (let e = 0; e < gameOfLife.gameWidth.length; e++) {
+                          gameOfLife.queueChange(i, e);
+                        }
+                      }
+                      gameOfLife.executeChanges();
+                    },
+
+                    queueChange: function queueChange(xCoord, yCoord) { // TODO write this
+                      // returns the number of adjacent "on" cells
+                      const numberOfAdjacent = gameOfLife.countAdjacent(xCoord, yCoord);
+
+                      if (numberOfAdjacent < 2 || numberOfAdjacent > 3) {
+                        gameOfLife.queueDeath(xCoord, yCoord);
+                      } else if (numberOfAdjacent === 3) {
+                        gameOfLife.queueLife(xCoord, yCoord);
+                      }
+                    },
+
+                    countAdjacent: function countAdjacent(xCoord, yCoord) {}, //TODO Make us
+                    queueLife: function queueLife(xCoord, yCoord) {}, // TODO Make us
+                    queueDeath: function queueDeath(xCoord, yCoord) {}, // TODO Make us
+                    executeChanges: function executeChanges() {}, // TODO Make us
+
+                    // does some very janky random crap, I should probably fix that, or not
+                    randomizeSeed: function randomizeSeed() {
+                      for (let i = 0; i < gameOfLife.gameCells.length; i++) {
+                        const diceRoll =
+                          Math.floor(Math.random() * (gameOfLife.seedDensity) - 0) + 0;
+
+                        if (diceRoll > gameOfLife.seedDensity / 100) {
+                          gameOfLife.setCellAlive(i);
+                        }
+                      }
+                    },
+
+                    // turns the cell "on"
+                    setCellAlive: function setCellAlive(cellNumber) {
+                      gameOfLife.gameCells[cellNumber].className = 'on';
+                    },
+
                     // kills the interval if running, runSimulation if not
                     togglePause: function togglePause() {
                       if (gameOfLife.isPaused === true) {
@@ -87,29 +136,6 @@ const gameOfLife = { isPaused: true,
                         clearInterval(gameOfLife.gameInterval);
                         gameOfLife.isPaused = true;
                       }
-                    },
-                    evaluateLife: function evaluateLife(grid) { // TODO: Make this work
-                      let xCoord = 0;
-                      let yCoord = 0;
-
-                      for (let i = 0; i < gameOfLife.gameCells.length; i++) {
-
-                      }
-                    },
-                    // does some very janky random crap, I should probably fix that, or not
-                    randomizeSeed: function randomizeSeed() { // TODO: Make this work
-                      for (let i = 0; i < gameOfLife.gameCells.length; i++) {
-                        const diceRoll =
-                          Math.floor(Math.random() * (gameOfLife.seedDensity ) - 0) + 0;
-
-                        if (diceRoll > gameOfLife.seedDensity / 100) {
-                          gameOfLife.setCellAlive(i);
-                        }
-                      }
-                    },
-                    // turns the cell "on"
-                    setCellAlive: function setCellAlive(cellNumber) {
-                      gameOfLife.gameCells[cellNumber].className = 'on';
                     },
 };
 
